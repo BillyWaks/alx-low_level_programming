379bc1b163e7dd95374080ecc0366adc292bd227
BillyWaks <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-07-17 14:20:48 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,27 +7,26 @@
 
 int main(void)
 {
-        char c;
+	char c;
 
 	int d;
 
-        c = 'a';
+	c = 'a';
 
 	d = 0;
 
-        while
-                (d < 10) {
-                        putchar(d + '0');
-                        d++;
-                }
-
 	while
-                (c <= 'f') {
-                        putchar(c);
-                        c++;
-                }
-        putchar('\n');
-        return(0);
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
 
 
