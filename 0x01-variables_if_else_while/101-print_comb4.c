9a8d4928bd4e8873e50ba40bc07a7bfe73eb6000
BillyWaks <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-07-17 14:58:45 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-        int ones;
-        int tens;
-	int hundreds;
+	int n, m, l;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (n = 48; n < 58; n++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
+		for (m = 49; m < 58; m++)
 		{
-			for (ones = '0'; ones <= '9'; ones++)
+			for (l = 50; l < 58; l++)
 			{
-				if (!((ones == tens) || (tens > ones) || (tens == hundreds) || (hundreds > tens)))
+				if (l > m && m > n)
 				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-
-					if (!(ones == '9') && (tens == '8') && (hundreds == '7'))
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
-						putchar(' ');	
+						putchar(' ');
 					}
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return(0);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
