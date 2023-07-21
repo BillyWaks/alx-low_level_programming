b76fdfcf7bc460c1a6ef2aa309b7691cf0834ca7
BillyWaks <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-07-21 04:49:49 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies integers
+ * @c: char to be checked
+ *
+ * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
