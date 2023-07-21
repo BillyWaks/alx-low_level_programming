b12ddf456961e230a52653a94ad49c41382be32d
BillyWaks <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-07-21 05:00:05 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *isupper - uppercase letters
- *@c: character to print
+ * _isupper - uppercase letters
+ * @c: char to check
  *
  * Return: 0 or 1
  */
@@ -10,9 +10,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
 		return (0);
 }
