88842d2cc9133ff70c990131637568fa04d6fb92
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-08-30 13:49:26 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
 
+void first(void) __attribute__ ((constructor));
 
-void ComeFirstbyCTC(void)__attribute__ ((constructor));
 /**
- * ComeFirstbyCTC - function that runs before the main
+ * first - prints before the main
+ * execute function
  */
 
-void ComeFirstbyCTC(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
+
