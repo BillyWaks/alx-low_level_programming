ab00a3ebc688bfdedd0825a7ed03a10804e4f2ea
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-08-30 13:21:35 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
+	/*initialization*/
 	size_t count = 0;
 
 	while (h)
 	{
-			if (!h->str)
-				printf("[0] (nil)\n");
-			else
-				printf("[%u]%s\n", h->len, h->str);
-
-			h = h->next;
-			count++;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
+
