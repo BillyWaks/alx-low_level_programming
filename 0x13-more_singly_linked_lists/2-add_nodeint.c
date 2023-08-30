6b71b70bfb28e09abfd615822ccf24ca68c5900a
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-08-30 02:01:18 -0700

    task 3

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+/**
+ * add_nodeint - function that adds a new node at the beginning of a listint_t list
+ * @head: double pointer to the header file
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
