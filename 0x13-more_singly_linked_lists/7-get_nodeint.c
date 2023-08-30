d2123439ec47931be8843498ba0d7174e193d022
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-08-30 07:12:01 -0700

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <strings.h>
+
+/**
+ * get_nodeint_at_index - returns the node at a certain index in a linked list
+ * @head: pointer to the first element in the list
+ * @index: index of the node to return
+ *
+ * Return: 0 when success, NULL when it fails
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	/* initialization */
+	unsigned int num = 0;
+
+	listint_t *node = head;
+
+	while (num < index && node)
+	{
+		node = node->next;
+		num++;
+	}
+
+	return (node ? node : NULL);
+}
