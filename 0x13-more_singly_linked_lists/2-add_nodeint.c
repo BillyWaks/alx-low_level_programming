11033db7229781176e1fe32709bf578ac1e0ac7e
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-08-30 02:14:27 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,8 +4,9 @@
 #include <strings.h>
 
 /**
- * add_nodeint - function that adds a new node at the beginning of a listint_t list
+ * add_nodeint - adds a new node at the beginning of a linked list
  * @head: double pointer to the header file
+ * @n: declares the integer value
  *
  * Return: the address of the new element, or NULL if it failed
  */
