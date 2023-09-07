0082dd89f5231f93c945ced4a48cf4367cc470aa
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2023-09-06 23:27:02 -0700

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,4 +11,35 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (len > 0)
+	{
+		ssize_t bytes_written = write(fd, text_content, len);
+
+		close(fd);
+
+		if (bytes_written != len)
+			return (-1);
+	}
+	else
+	{
+		close(fd);
+	}
+	return (1);
+}
+
 
